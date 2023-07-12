775a78ff711eba3dff6965f7c0fe5d136f2a0545
dogmanss <975965279@qq.com>
2023-07-12 22:32:01 +0800

    连接器

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,12 @@ import "./globals.css";
 import { polygonMumbai } from "wagmi/chains";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
+import { InjectedConnector } from "wagmi/connectors/injected";
 import { LensProvider, LensConfig, production, appId, development } from "@lens-protocol/react-web";
 import { bindings as wagmiBindings } from "@lens-protocol/wagmi";
 import "@/app/react-i18next/i18n";
 
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
-import { InjectedConnector } from 'wagmi/connectors/injected'
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
-
-const { chains, publicClient, webSocketPublicClient } = configureChains(
+const { publicClient, webSocketPublicClient } = configureChains(
   [polygonMumbai],
   [publicProvider()]
 );
@@ -23,24 +19,9 @@ const config = createConfig({
   publicClient,
   webSocketPublicClient,
   connectors: [
-    new MetaMaskConnector({ chains }),
-    new CoinbaseWalletConnector({
-      chains,
-      options: {
-        appName: 'wagmi',
-      },
-    }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        projectId: '...',
-      },
-    }),
     new InjectedConnector({
-      chains,
       options: {
-        name: 'Injected',
-        shimDisconnect: true,
+        shimDisconnect: false,
       },
     }),
   ],
