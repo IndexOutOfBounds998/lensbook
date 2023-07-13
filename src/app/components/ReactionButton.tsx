db53177dc3f0fc454aa477eb88dbf9d5aac5c484
dogmanss <975965279@qq.com>
2023-07-14 00:06:50 +0800

    国际化

--- src/app/components/ReactionButton.tsx
+++ src/app/components/ReactionButton.tsx
@@ -9,10 +9,10 @@ import {
 
 } from '@lens-protocol/react-web';
 
-import {Button, message} from 'antd';
-import {useState} from "react";
+import { Button, message } from 'antd';
+import { useState } from "react";
 import './style/ReactionButton.css'
-
+import { useTranslation } from "react-i18next";
 type ReactionButtonProps = {
     publication: ContentPublication;
     profileId: ProfileId;
@@ -20,6 +20,8 @@ type ReactionButtonProps = {
 };
 
 export default function ReactionButton({ publication, profileId, reactionType }: ReactionButtonProps) {
+
+    const { t } = useTranslation();
 
     const { addReaction, removeReaction, hasReaction, isPending } = useReaction({
         profileId,
@@ -56,13 +58,13 @@ export default function ReactionButton({ publication, profileId, reactionType }:
                 onClick={toggleReaction}
                 type="link"
                 loading={loading}
-                title={hasReactionType ? `取消点赞` : `点赞`}
+                title={hasReactionType ? t('unlike') : t('like')}
                 ghost
                 disabled={isPending || (hasAnyReaction && !hasReactionType)}
                 icon={<i className={`iconfont icon-${hasReactionType ? 'heart-fill' : 'heart'} cursor-pointer text-[25px] mr-3`} />}
                 className='reaction-btn flex items-center p-[0] h-[auto] text-[#ff2442]'
             >
-                <span className='font-bold h-[25px]'>{ publication.stats.totalUpvotes }</span>
+                <span className='font-bold h-[25px]'>{publication.stats.totalUpvotes}</span>
             </Button>
         </>
     );
