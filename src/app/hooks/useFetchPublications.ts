9360cb67e3a05b61162e755a2adc752673996c51
dogmanss <975965279@qq.com>
2023-07-17 01:32:25 +0800

    useFetchPublications

--- src/app/hooks/useFetchPublications.ts
+++ src/app/hooks/useFetchPublications.ts
@@ -1,57 +1,84 @@
-
-import { useState } from 'react';
+import { useState, useEffect,useRef } from 'react';
 import { ExplorePublicationRequest } from '@lens-protocol/client';
 import { getAuthenticatedClient } from '../shared/getAuthenticatedClient';
+import { isEqual } from 'lodash';
 type useFetchPublicationsArgs = {
-    explorePublicationRequest: ExplorePublicationRequest;
+  explorePublicationRequest: ExplorePublicationRequest;
 };
 
+// 使用方式
+// const explorePublicationRequest = {
+//     cursor: JSON.stringify({
+//       timestamp: 1,
+//       offset: 20,
+//       filter: JSON.stringify({
+//         mainContentFocus: ['IMAGE']
+//       })
+//     }),
+//     sortCriteria: PublicationSortCriteria.Latest,
+//     limit: 20,
+//     publicationTypes: [PublicationTypes.Post],
+//     sources: ['lenster', 'lenstrip']
+//   };
+  
+//   const { data, loading, hasMore, next ,reset} = useFetchPublications({
+//     explorePublicationRequest
+//   });
+
 export function useFetchPublications({
-    explorePublicationRequest
+  explorePublicationRequest,
 }: useFetchPublicationsArgs) {
-    const [loading, setLoading] = useState(false);
-    const [data, setData] = useState([]);
-    const [nextCursor, setNextCursor] = useState('');
-    const [hasmore, setHasmore] = useState(false);
-    const [request, setRequest] = useState(undefined);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [nextCursor, setNextCursor] = useState('');
+  const [hasMore, setHasMore] = useState(false);
+  const [request, setRequest] = useState(undefined);
+  const prevExplorePublicationRequest = useRef<ExplorePublicationRequest>();
 
+  const execute = async () => {
+    setRequest(explorePublicationRequest);
+    setLoading(true);
+    const lensClient = await getAuthenticatedClient();
+    let res = await lensClient.explore.publications(explorePublicationRequest);
+    setLoading(false);
+    setData(prevData => [...prevData, ...res.items]);
+    setNextCursor(res.pageInfo.next);
+    setHasMore(res.items.length > 0);
+  };
 
-    const execute = async () => {
-        setRequest(explorePublicationRequest);
-        setLoading(true);
-        const lensClient = await getAuthenticatedClient();
-        let res =await lensClient.explore.publications(explorePublicationRequest);
-        setLoading(false);
-        debugger
-        setData(prevData => [...prevData, ...res.data]);
-        setNextCursor(res.next)
-        if(res.data.length==0){
-            setHasmore(false);
-        }else{
-            setHasmore(true);
-        }
-    };
+  useEffect(() => {
+    if (
+      !prevExplorePublicationRequest.current ||
+      !isEqual(
+        prevExplorePublicationRequest.current,
+        explorePublicationRequest
+      )
+    ) {
+      execute();
+    }
 
-    const next = async () => {
-        setLoading(true);
-        const lensClient = await getAuthenticatedClient();
-        request.cursor=nextCursor;
-        let res =await lensClient.explore.publications(request);
-        setLoading(false);
-        setData(prevData => [...prevData, ...res.data]);
-        setNextCursor(res.next)
-    };
+    prevExplorePublicationRequest.current = explorePublicationRequest;
+  }, [explorePublicationRequest]);
 
-    const reset = async () => {
-        setData([]);
-    };
+  const next = async () => {
+    setLoading(true);
+    const lensClient = await getAuthenticatedClient();
+    request.cursor = nextCursor;
+    let res = await lensClient.explore.publications(request);
+    setLoading(false);
+    setData(prevData => [...prevData, ...res.items]);
+    setNextCursor(res.pageInfo.next);
+  };
 
-    return {
-        execute,
-        next,
-        reset,
-        hasmore,
-        data,
-        loading
-    };
+  const reset = async () => {
+    setData([]);
+  };
+
+  return {
+    next,
+    reset,
+    hasMore,
+    data,
+    loading,
+  };
 }
