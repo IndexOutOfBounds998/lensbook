60a2b34573983ba0feb660fddef7ac6ddca32c67
dogmanss <975965279@qq.com>
2023-07-17 19:39:39 +0800

    notice

--- src/app/pages/Home/NoteDetail.tsx
+++ src/app/pages/Home/NoteDetail.tsx
@@ -222,161 +222,163 @@ export default function NoteDetail({ card, img, item, setShowDetail }) {
         { icon: isCollectionStatus ? 'star-fill' : 'star', text: collectionCount, color: '#ec4899', onClick: clickCollection }
     ];
 
-    if (publication_loading) {
-        return <Skeleton />;
-    }
+
 
 
     return (
         <>
             {contextHolder}
             {contextHolderNotification}
-            <div
-                className='h-full w-full fixed top-0 z-[100]'
-                style={{
-                    background: show ? '' : 'hsla(0,0%,100%,.98)',
-                    transition: 'all 0.5s ease'
-                }}
-                onClick={() => {
-                    setShow(true);
-                    scaleDown();
-                    setTimeout(() => { setShowDetail(false) }, 500)
-                }}
-            >
+            
                 <div
-                    ref={detail}
-                    className='h-[calc(100%-64px)] w-[70%] my-[32px] flex rounded-3xl overflow-hidden'
-                    onClick={(e) => {
-                        e.stopPropagation();
+                    className='h-full w-full fixed top-0 z-[100]'
+                    style={{
+                        background: show ? '' : 'hsla(0,0%,100%,.98)',
+                        transition: 'all 0.5s ease'
+                    }}
+                    onClick={() => {
+                        setShow(true);
+                        scaleDown();
+                        setTimeout(() => { setShowDetail(false) }, 500)
                     }}
                 >
+                    <Skeleton loading={publication_loading} >
                     <div
-                        className='w-[auto] bg-[#f8f8f8] flex'
-                        style={{
-                            background: show ? 'rgba(233,245,250,0)' : '',
-                            borderRadius: show ? 'none' : '1rem',
+                        ref={detail}
+                        className='h-[calc(100%-64px)] w-[70%] my-[32px] flex rounded-3xl overflow-hidden'
+                        onClick={(e) => {
+                            e.stopPropagation();
                         }}
                     >
                         <div
-                            ref={imgBox}
-                            className='bg-no-repeat bg-contain h-full'
+                            className='w-[auto] bg-[#f8f8f8] flex'
                             style={{
-                                backgroundImage: `url(${img && img.src})`,
-                                width: `${bgSize}px`
+                                background: show ? 'rgba(233,245,250,0)' : '',
+                                borderRadius: show ? 'none' : '1rem',
                             }}
                         >
-                        </div>
-                    </div>
-                    <div
-                        ref={contentBox}
-                        style={{ display: show ? 'none' : '', transition: 'none' }}
-                        className='bg-white mb-[131px] w-full'
-                    >
-                        <div
-                            className='flex py-5 px-6 justify-between'
-                            style={{ borderBottom: '0.5px solid rgba(0,0,0,.1)' }}
-                        >
-                            <div className='flex items-center'>
-                                <img
-                                    className='rounded-3xl w-[40px] h-[40px] mx-2'
-                                    src={publication && publication.profile.picture ? formatPicture(publication && publication.profile.picture) : user}
-                                    alt=""
-                                />
-                                <span>{publication && publication.profile.name ? publication && publication.profile.name : formatNickName(publication && publication.profile.handle)}</span>
-                            </div>
-
-
-                            <WhenLoggedInWithProfile>
-                                {({ profile }) =>
-                                    <FollowButton followee={publication && publication.profile} follower={profile} />}
-                            </WhenLoggedInWithProfile>
-
-
-                        </div>
-                        <div id='noteDetail' className='h-[calc(100%-81px)] overflow-auto'>
-                            <InfiniteScroll
-                                dataLength={commentPage.length}
-                                next={moreComment}
-                                hasMore={hasMore}
-                                loader={
-                                    <div
-                                        className='w-full flex items-center justify-center h-[50px] mb-[20px]'
-                                    >
-                                        <Spin tip="Loading" size="large" />
-                                        <span className='text-[13px] ml-[15px]'>加载中</span>
-                                    </div>
-                                }
-                                scrollableTarget='noteDetail'
+                            <div
+                                ref={imgBox}
+                                className='bg-no-repeat bg-contain h-full'
+                                style={{
+                                    backgroundImage: `url(${img && img.src})`,
+                                    width: `${bgSize}px`
+                                }}
                             >
-                                <div
-                                    className='mx-[30px] pb-[30px] pt-[10px]'
-                                    style={{ borderBottom: '0.5px solid rgba(0,0,0,.1)' }}
-                                >
-                                    <div className='mb-5 font-semibold text-[20px] leading-8'>
-                                        {publication && publication.metadata.title && publication && publication.metadata.title}
-                                    </div>
-                                    <div
-                                        className='text-[17px] whitespace-pre-wrap'
-                                        dangerouslySetInnerHTML={{ __html: publication && publication.metadata.content && publication && publication.metadata.content }}
-                                    >
-                                    </div>
-                                    <div className='mt-2 text-[14px] leading-6 text-[#33333399]'>
-                                        {formatDate(publication && publication.createdAt)}
-                                    </div>
-                                </div>
-                                <div className='px-[30px] py-[20px]'>
-                                    <div className='mt-[16px]'>
-                                        <Comment
-                                            ref={appendReplyRef}
-                                            item={commentPage}
-                                            total={item.stats ? item.stats.totalAmountOfComments : 0}
-                                        >
-                                        </Comment>
-                                    </div>
-                                </div>
-                            </InfiniteScroll>
+                            </div>
                         </div>
-                        <footer
-                            className='border-t fixed bottom-[0] bg-white py-[10px] px-[30px]'
-                            style={{ width: `${contentSize}px` }}
+                        <div
+                            ref={contentBox}
+                            style={{ display: show ? 'none' : '', transition: 'none' }}
+                            className='bg-white mb-[131px] w-full'
                         >
-                            <div className='h-[50px] rounded-3xl shadow w-full mb-[10px] py-[7px] px-[15px] flex justify-between'>
-
-
-                                <WhenLoggedInWithProfile>
-                                    {({ profile }) =>
-                                        <ReactionButton publication={publication} profileId={profile.id} reactionType={ReactionType.UPVOTE} />}
-                                </WhenLoggedInWithProfile>
-
-                                <WhenLoggedInWithProfile>
-                                    {({ profile }) =>
-                                        <CollectButton collector={profile} publication={publication} />}
-                                </WhenLoggedInWithProfile>
-
-
-
-                                <div className='w-[70px] rounded-3xl bg-[#50b674] cursor-pointer p-[5px] text-[#fff] text-center'>
-                                    分享
-                                </div>
-                            </div>
-                            <div className='h-[50px] rounded-3xl w-full flex'>
-                                <div className='w-[calc(100%-95px)] mr-[15px] shadow p-[5px] h-[50px] bg-[#f9f9f9] rounded-3xl'>
-                                    <Input
-                                        ref={commentRef}
-                                        className='h-full text-[16px]'
-                                        placeholder="期待您的精彩评论"
-                                        bordered={false}
+                            <div
+                                className='flex py-5 px-6 justify-between'
+                                style={{ borderBottom: '0.5px solid rgba(0,0,0,.1)' }}
+                            >
+                                <div className='flex items-center'>
+                                    <img
+                                        className='rounded-3xl w-[40px] h-[40px] mx-2'
+                                        src={publication && publication.profile.picture ? formatPicture(publication && publication.profile.picture) : user}
+                                        alt=""
                                     />
+                                    <span>{publication && publication.profile.name ? publication && publication.profile.name : formatNickName(publication && publication.profile.handle)}</span>
                                 </div>
-                                <Button loading={messageReplyButtonLoading} className='h-full bg-[#6790db] cursor-pointer w-[80px] rounded-3xl flex justify-center items-center text-[#fff] text-[16px]'
-                                    onClick={sendComment}>
-                                    发送
-                                </Button>
+
+
+                                <WhenLoggedInWithProfile>
+                                    {({ profile }) =>
+                                        <FollowButton followee={publication && publication.profile} follower={profile} />}
+                                </WhenLoggedInWithProfile>
+
+
                             </div>
-                        </footer>
+                            <div id='noteDetail' className='h-[calc(100%-81px)] overflow-auto'>
+                                <InfiniteScroll
+                                    dataLength={commentPage.length}
+                                    next={moreComment}
+                                    hasMore={hasMore}
+                                    loader={
+                                        <div
+                                            className='w-full flex items-center justify-center h-[50px] mb-[20px]'
+                                        >
+                                            <Spin tip="Loading" size="large" />
+                                            <span className='text-[13px] ml-[15px]'>加载中</span>
+                                        </div>
+                                    }
+                                    scrollableTarget='noteDetail'
+                                >
+                                    <div
+                                        className='mx-[30px] pb-[30px] pt-[10px]'
+                                        style={{ borderBottom: '0.5px solid rgba(0,0,0,.1)' }}
+                                    >
+                                        <div className='mb-5 font-semibold text-[20px] leading-8'>
+                                            {publication && publication.metadata.title && publication && publication.metadata.title}
+                                        </div>
+                                        <div
+                                            className='text-[17px] whitespace-pre-wrap'
+                                            dangerouslySetInnerHTML={{ __html: publication && publication.metadata.content && publication && publication.metadata.content }}
+                                        >
+                                        </div>
+                                        <div className='mt-2 text-[14px] leading-6 text-[#33333399]'>
+                                            {formatDate(publication && publication.createdAt)}
+                                        </div>
+                                    </div>
+                                    <div className='px-[30px] py-[20px]'>
+                                        <div className='mt-[16px]'>
+                                            <Comment
+                                                ref={appendReplyRef}
+                                                item={commentPage}
+                                                total={item.stats ? item.stats.totalAmountOfComments : 0}
+                                            >
+                                            </Comment>
+                                        </div>
+                                    </div>
+                                </InfiniteScroll>
+                            </div>
+                            <footer
+                                className='border-t fixed bottom-[0] bg-white py-[10px] px-[30px]'
+                                style={{ width: `${contentSize}px` }}
+                            >
+                                <div className='h-[50px] rounded-3xl shadow w-full mb-[10px] py-[7px] px-[15px] flex justify-between'>
+
+
+                                    <WhenLoggedInWithProfile>
+                                        {({ profile }) =>
+                                            <ReactionButton publication={publication} profileId={profile.id} reactionType={ReactionType.UPVOTE} />}
+                                    </WhenLoggedInWithProfile>
+
+                                    <WhenLoggedInWithProfile>
+                                        {({ profile }) =>
+                                            <CollectButton collector={profile} publication={publication} />}
+                                    </WhenLoggedInWithProfile>
+
+
+
+                                    <div className='w-[70px] rounded-3xl bg-[#50b674] cursor-pointer p-[5px] text-[#fff] text-center'>
+                                        分享
+                                    </div>
+                                </div>
+                                <div className='h-[50px] rounded-3xl w-full flex'>
+                                    <div className='w-[calc(100%-95px)] mr-[15px] shadow p-[5px] h-[50px] bg-[#f9f9f9] rounded-3xl'>
+                                        <Input
+                                            ref={commentRef}
+                                            className='h-full text-[16px]'
+                                            placeholder="期待您的精彩评论"
+                                            bordered={false}
+                                        />
+                                    </div>
+                                    <Button loading={messageReplyButtonLoading} className='h-full bg-[#6790db] cursor-pointer w-[80px] rounded-3xl flex justify-center items-center text-[#fff] text-[16px]'
+                                        onClick={sendComment}>
+                                        发送
+                                    </Button>
+                                </div>
+                            </footer>
+                        </div>
                     </div>
-                </div>
-            </div >
+                    </Skeleton>
+                </div >
+          
         </>
     );
 }
