c1503245977a26fd165a8e77680c99021ec34cd0
dogmanss <975965279@qq.com>
2023-07-22 22:03:45 +0800

    nextjs neo

--- src/app/pages/layout/Header.tsx
+++ src/app/pages/layout/Header.tsx
@@ -7,7 +7,7 @@ import Search from "./Search";
 import { useActiveProfile } from '@lens-protocol/react-web';
 import { useActiveWallet } from '@lens-protocol/react-web';
 import RegisterButton from "@/app/components/button/RegisterButton";
-
+import { NextSeo } from 'next-seo';
 export default function Header({ }) {
 
     const { data: wallet, loading } = useActiveWallet();
@@ -26,26 +26,32 @@ export default function Header({ }) {
     // }
 
     return (
-        <header className='w-full px-10 fixed bg-white top-0 min-w-[1250px] z-50'>
-            <div className='items-center flex justify-between'>
-                <div className='w-20'>
-                    <img className='w-20 h-16' src={logo} alt="" />
+        <>
+            <NextSeo
+                title="LensBook"
+                description="LensBook is a decentralized and permissionless social media app built with Lens Protocol 🌿."
+            />
+            <header className='w-full px-10 fixed bg-white top-0 min-w-[1250px] z-50'>
+                <div className='items-center flex justify-between'>
+                    <div className='w-20'>
+                        <img className='w-20 h-16' src={logo} alt="" />
+                    </div>
+                    <div className='w-[30%] h-10 fixed left-1/2 translate-x-[-50%]'>
+                        <Search />
+                    </div>
+                    <div className='flex items-center'>
+                        {titleList.map((item, index) => (
+                            <span className='text-[16px] m-4 text-zinc-500 cursor-pointer' key={index}>
+                                <a href={item.src}>{item.text}</a>
+                            </span>
+                        ))}
+                        {wallet ?
+                            profile ? <ProfileSetting /> : <RegisterButton /> : <LoginButton />
+                        }
+                    </div>
                 </div>
-                <div className='w-[30%] h-10 fixed left-1/2 translate-x-[-50%]'>
-                    <Search />
-                </div>
-                <div className='flex items-center'>
-                    {titleList.map((item, index) => (
-                        <span className='text-[16px] m-4 text-zinc-500 cursor-pointer' key={index}>
-                            <a href={item.src}>{item.text}</a>
-                        </span>
-                    ))}
-                    {wallet ?
-                        profile ? <ProfileSetting /> : <RegisterButton /> : <LoginButton />
-                    }
-                </div>
-            </div>
 
-        </header>
+            </header>
+        </>
     )
 }
