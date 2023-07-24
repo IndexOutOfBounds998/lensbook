345efa4f6a6b8f80973b0c893c9a2f8c3f97e5b2
dogmanss <975965279@qq.com>
2023-07-24 19:38:39 +0800

    no export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     reactStrictMode: true,
     webpack: config => {
         config.resolve.fallback = { fs: false, net: false, tls: false };
