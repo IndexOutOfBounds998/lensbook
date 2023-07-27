70303ee49c1d81faafbc45bdcf6b37dead332909
youjiaqi <jiaqi.you@luttica.com>
2023-07-27 11:14:31 +0800

    视频发布

--- src/app/components/uploadVideo/VideoModel.tsx
+++ src/app/components/uploadVideo/VideoModel.tsx
@@ -1,19 +1,25 @@
 "use client";
-import { Radio, Input, Button } from 'antd';
+import {Radio, Input, Button, Upload, message} from 'antd';
 import React, { useEffect, useRef, useState } from "react";
 import { usePost } from '../../hooks/usePost'
 import { useTranslation } from "react-i18next";
-import MuUploadImagButton from "../../components/button/MuUploadImagButton";
-import i18n from "i18next";
 import type { UploadFile } from 'antd/es/upload/interface';
-import { uuid } from "@walletconnect/legacy-utils";
-import { PublicationMetadataV2Input, PublicationMainFocus, PublicationMetadataDisplayTypes } from '@lens-protocol/client';
 import {
     useActiveProfile
 } from '@lens-protocol/react-web';
 const { TextArea } = Input;
+import { useSearchParams  } from 'next/navigation'
+import ReactPlayer from "react-player";
+import {RcFile, UploadProps} from "antd/es/upload";
+import i18n from "i18next";
+import {uuid} from "@walletconnect/legacy-utils";
+import {PublicationMetadataV2Input, PublicationMainFocus, PublicationMetadataDisplayTypes} from "@lens-protocol/client";
+import {useUpIpfs} from "../../hooks/useUpIpfs";
+import { getAuthenticatedClient } from "@/app/shared/getAuthenticatedClient";
 
-export default function Page() {
+const VideoModel: React.FC<{
+    videoData: any;
+}> = ({ videoData }) =>  {
 
     const { t } = useTranslation();
 
@@ -24,12 +30,20 @@ export default function Page() {
     let [stateValue, setStateValue] = useState('public');
     let [timeValue, setTimeValue] = useState('just');
     let [quillRef, setQuillRef] = useState();
+    let [cover, setCover] = useState('');
 
-    const [fileList, setFileList] = useState<UploadFile[]>([]);
-    // let quillRef = useRef(undefined);
     let titleRef = useRef(undefined);
 
     const { data: profile, error, loading: profileLoading } = useActiveProfile();
+
+    const props: UploadProps = {
+        name: 'file',
+        action: '/api/upload',
+        showUploadList: false,
+        beforeUpload(file, FileList) {
+            beforeUpload(file);
+        },
+    };
 
     const showModal = () => {
         setIsModalOpen(true);
@@ -41,44 +55,33 @@ export default function Page() {
         alert("发布失败" + error)
     }
 
+    const { execute, loading: ipfsLoading, url } = useUpIpfs({ type: 'upLoadImg' });
+    const beforeUpload = async (file: RcFile) => {
+        const formData = new FormData();
+        formData.append('file', file);
+        const url = await execute(formData);
+        if (url) {
+            setCover(url);
+        }
+    };
+
     const { submit: post, postLoading } = usePost(callbackOnSuccess, callbackOnError);
 
     const onSubmit = async () => {
-
-        if (fileList && fileList.length === 0) {
-            alert("请上传图片");
-            return
-        }
         if (!titleRef.current || !quillRef) {
             alert("请填写内容");
             return
         }
-        //处理图片
-        let images = fileList.filter((item) => {
-            return item.response.code === 200;
-        }).map((item) => {
 
-            return {
-                cover: "ipfs://" + item.response.data,
-                item: "ipfs://" + item.response.data,
-                type: item.type
-            }
+        const lensClient = await getAuthenticatedClient();
+        const result = await lensClient.publication.createAttachMediaData({
+            itemCid: videoData.cid,
+            type: videoData.type,
+            altTag: "video test",
+            cover: "ipfs://" + cover,
+        });
 
-        })
-
-        //处理属性 让 nft 更加唯一性
-
-        let attributes = fileList.filter((item) => {
-            return item.response.code === 200;
-        }).map((item) => {
-
-            return {
-                displayType: PublicationMetadataDisplayTypes.Number,
-                traitType: "size",
-                value: item.size
-            }
-
-        })
+        console.log(result)
 
         let current_locale = i18n.language
 
@@ -86,26 +89,24 @@ export default function Page() {
             version: "2.0.0",
             metadata_id: uuid(),
             appId: "lenstrip",
-            image: images[0].item,
-            imageMimeType: images[0].type,
+            image: "ipfs://" + cover,
             content: quillRef,
             title: titleRef.current.input.value,
-            attributes: attributes,
+            attributes: [
+                {
+                    displayType: PublicationMetadataDisplayTypes.Number,
+                    traitType: "size",
+                    value: videoData.size
+                }
+            ],
             locale: current_locale,
             mainContentFocus: PublicationMainFocus.Image,
-            media: images,
+            media: result,
             tags: ["trip"],
             name: `Post by ${profile.handle}`
         }
-        //    try {
-        //      await ValidateMetadata(matedata);
-        //    } catch (error) {
-        //        alert("matadata 不标准");
-        //        return
-        //    }
+
         post(matedata);
-
-
     };
 
     return (
@@ -113,12 +114,34 @@ export default function Page() {
             <div className="w-full h-full bg-white overflow-y-scroll p-[30px] pl-[30px]">
                 <div className="text-[20px] flex items-center">
                     <div className="w-[10px] h-[25px] inline-block bg-[blueviolet] rounded-3xl mr-1" />
-                    <span>{t('publishTitle')}</span>
+                    <span>{t('postVideo')}</span>
                 </div>
                 <div className="p-[20px]">
                     <div className="mb-[10px]">
-                        {/* <p className="mb-[10px] text-[18px]">{t('imageEditing')}&nbsp;&nbsp;<span className="text-[14px] text-[blue]">+{t('uploadMore')}</span></p> */}
-                        <MuUploadImagButton fileList={fileList} setFileList={setFileList}></MuUploadImagButton>
+                        <div className="mb-[10px]">
+                            <span className="mb-[10px] text-[18px]">{t('videoEditing')}</span>
+                            <Upload
+                                {...props}
+                            >
+                                <Button
+                                    type="primary"
+                                    size='small'
+                                    className='bg-[blueviolet] ml-[15px]'
+                                >
+                                    设置封面
+                                </Button>
+                            </Upload>
+                        </div>
+                        <div  className='w-[200px] h-[300px] border'>
+                            <ReactPlayer
+                                url={videoData.url}
+                                controls
+                                loop
+                                light={cover ? <img src={`https://ipfs.io/ipfs/${cover}`}/> : false}
+                                width='100%'
+                                height='100%'
+                            />
+                        </div>
                     </div>
                     <div className="mb-[16px]">
                         <Input
@@ -158,15 +181,6 @@ export default function Page() {
                     </div>
                     <div className="text-[13px]">
                         <p className="text-[18px] text-[#ea9d4e] my-[25px]">{t('publishSetting')}</p>
-                        <div className='w-[500px] flex items-center mb-[20px]'>
-                            {/* <span className="text-[14px] mr-3">{t('addLoc')}</span>
-                            <div className='min-w-[350px]'>
-                                <Input
-                                    className=""
-                                    placeholder={t('pleaseSelect')}
-                                />
-                            </div> */}
-                        </div>
                         <div className='mb-[20px]'>
                             <span className="text-[14px] mr-3">{t('permission')}</span>
                             <Radio.Group
@@ -221,3 +235,4 @@ export default function Page() {
         </div>
     );
 }
+export default VideoModel;
